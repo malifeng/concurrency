cbb24131b153affd1cff29063e45254739cf967e
mlf <lifeng.ma@unicareer.com>
2020-04-16 14:03:50 +0800

    添加注解

---
+++ src/main/java/com/mlf/concurrency/annocations/NotThreadSafe.java
@@ -0,0 +1,16 @@
+package com.mlf.concurrency.annocations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+/**
+ * 用来标记线程【不安全】的类或者写法
+ */
+
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.SOURCE)
+public @interface NotThreadSafe {
+    String value() default "";
+}
